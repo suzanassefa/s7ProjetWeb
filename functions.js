bfd8e62567470e1f60e4314dbfbd157fd6d2da50
suzanassefa <80387624+suzanassefa@users.noreply.github.com>
2021-12-06 04:48:18 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,174 @@
+"use strict"
+// calcule a**(N-1) ≡ 1 mod N la condition de fermat
+
+function my_powMod(a, b, n){
+  return (a ** b) % n;
+}
+
+function my_pow(a,b){
+  return a**b;
+}
+
+ function my_abs(a){
+   if(a < 0){
+     a = a * -1n;
+   }
+   return a
+ }
+
+function euclid(a, b){
+  a = my_abs(a);
+  b = my_abs(b);
+  let r = 0
+  while (b != 0n){
+    r =  a % b ;
+    a = b;
+    b = r
+  }
+  return a
+}
+
+/// coefficient de bezout, algoritme de euclide étendu
+function extended_euclid(a, b){
+  let u0 = 1n
+  let u1 = 0n
+  let v0 = 0n
+  let v1 = 1n
+  let q = 0n
+  let r = 0n
+  let new0 = 0n
+  let new1 = 0n
+
+
+  while( b != 0n){
+    q = a / b
+    r = a % b
+    a = b
+    b = r
+    new0 = u1
+    new1 = u0-q*u1
+    u0 = new0
+    u1 = new1
+    new0 = v1
+    new1 = v0-q*v1
+    v0 = new0
+    v1 = new1
+
+  }
+
+  if(a >= 0n){
+    return [a, u0, v0]
+  }
+  else {
+    return [-a, -u0, -v0]
+  }
+}
+
+
+function modular_inverse(e, phin){
+  let extend_ecl = extended_euclid(e, my_abs(phin));
+  if(extend_ecl[0] == 1){
+    return (extend_ecl[1]+my_abs(phin))%my_abs(phin)
+  }
+  else{
+    return 0n
+  }
+}
+
+
+// test de primalite de Fermat avec n un entier positve
+function testPrimaliteFermat(n,k){
+  let a = generate_number(k);
+  while  (a >= n ){
+    a = generate_number(k);
+  }
+  if(my_powMod(a, n-1n, n) == 1n){
+    return true;
+  }
+  return false;
+}
+
+// générer un nombre aléatoire de k bit
+function generate_number(k){
+  let chaine_cara = ""
+  for (let i=0; i<k; i++){
+    const n = Math.floor(Math.random() * 9) ;
+    chaine_cara += n;
+  }
+  return BigInt(chaine_cara);
+}
+
+// générer un nb premier de k bit
+function generate_prime(k) {
+	let n = generate_number(k);
+    while (!testPrimaliteFermat(n,k)){
+      n = generate_number(k);
+	}
+	return n
+}
+
+
+
+function generate_key(k){
+	let n = Math.pow(2, k+1);
+	let p = 1;
+	let q =  1;
+  // on construi n
+	//while(n >= Math.pow(2,k) || n < Math.pow(2, k-1)  || q  == p){
+  while(n < 26 || q  == p){
+		 p = generate_prime(Math.floor(k/2));
+		 q = generate_prime(Math.floor(k/2));
+		 n = p*q ;
+	}
+  // on calcule phin
+	let phin  = (p-1n) * (q-1n);
+
+	let e = generate_number(Math.floor(k/2));
+  let may_e = 0;
+  while(euclid(e, phin)!=1n){
+    may_e = generate_number(Math.floor(k/2));
+    if(may_e>=2 && may_e<phin) {
+      e  = may_e;
+    }
+  }
+
+	let d =  modular_inverse(e, phin);
+
+	let result =  [p, q, n, phin, d, e];
+	return result;
+
+}
+
+function tester(k){
+  let k2 = Math.floor(k/2);
+  let [p,q,n,phin,d,e] =  generate_key(k);
+  console.log( "[p,q,n,phin,d,e]=generate_key(",k,")");
+  console.log( "                =", [p,q,n,phin,d,e]);
+  console.log( "p premier:", testPrimaliteFermat(p, k2));
+  console.log("q premier:", testPrimaliteFermat(q, k2));
+  //console.log( "p et q ont ",k2," bits:", p < Math.pow(2,k2) && p >= Math.pow(2,k2-1) && q < Math.pow(2,k2) && q >= Math.pow(2,k2-1));
+  console.log( "pq == n : ", p*q==n);
+  //console.log( "n a ",k," bits:", n<2**k && n>=2**(k-1));
+  console.log( "(p-1)*(q-1) == phin : ", (p-1n)*(q-1n) == phin);
+  console.log( "PGCD(e,phin)==1: ", euclid(e, phin) == 1);
+  console.log( "ed =1 mod phin", (e*d)% phin  == 1n );
+}
+
+//codeur et décodeur
+function square_and_multiply(m, e, n){
+  let step = my_pow(m,2n);
+  for (let i=2n; i<e; i++){
+    step = (m*step) % n
+  }
+  return step
+}
+
+function coder_mot(){
+  let saisie = document.getElementById("mot_cle").value;
+  console.log(saisie);
+  afficherMot_coder(37);
+}
+
+function decoder_mot(dechiffrer){
+afficherMot_decoder(document.getElementById("mot_cle").value);
+}
---
+++ main.js
@@ -0,0 +1,134 @@
+"use strict"
+
+let les_cles_dalice = generate_key(4);
+console.log("[p,q,n,phin,d,e] = ", les_cles_dalice);
+
+
+function diplayConnection(){
+  document.getElementById("contentPconnection").style.display = "block";
+  if (document.getElementById("contentDialogue").style !="none"){
+    document.getElementById("contentDialogue").style.display = "none";
+  }
+}
+
+function displayDiscussion(){
+  document.getElementById("contentDialogue").style.display = "block";
+  if (document.getElementById("contentPconnection").style !="none"){
+    document.getElementById("contentPconnection").style.display = "none";
+  }
+}
+
+function contenue_chaque_button(id, cours, resultat){
+  let div = document.getElementById(id);
+  if(div.childNodes.length == 0){
+    let p = document.createElement('p');
+    p.setAttribute('id', "cours");
+    let t = document.createTextNode(cours);
+    let p2 = document.createElement('p');
+    p2.setAttribute('id', "resultat");
+    let t2 = document.createTextNode(resultat);
+    p.appendChild(t);
+    p2.appendChild(t2);
+    div.appendChild(p);
+    div.appendChild(p2);
+  }
+}
+function affichage_prochain_button(id, def, attribut, value){
+  let div = document.getElementById(id);
+  let button = document.createElement("button");
+  button.setAttribute("class", "button");
+  button.setAttribute("onCLick", def+"("+attribut+")");
+  let t = document.createTextNode(value);
+  button.appendChild(t);
+  div.appendChild(button);
+}
+
+function nb_premier(){
+  let cours = "lorium isum dot set amet";
+  let resultat = "p = "+les_cles_dalice[0]+" et q = "+ les_cles_dalice[1];
+  contenue_chaque_button("nb_premier", cours, resultat);
+  affichage_prochain_button('nb_premier', "n_et_f", "", "Construire n et f");
+}
+
+function n_et_f(){
+  let cours = "lorium isum dot set amet";
+  let resultat = "n = "+les_cles_dalice[2]+" et f = "+ les_cles_dalice[3];
+  contenue_chaque_button("n_et_f", cours, resultat);
+  affichage_prochain_button('n_et_f', "e", "", "Construire e");
+
+}
+function e(){
+  let cours = "lorium isum dot set amet";
+  let resultat = "e = "+les_cles_dalice[5];
+  contenue_chaque_button("e", cours, resultat);
+  affichage_prochain_button('e', "d", "", "Construire d");
+
+}
+
+function d(){
+  let cours = "lorium isum dot set amet";
+  let resultat = "d = "+les_cles_dalice[4];
+  contenue_chaque_button("d", cours, resultat);
+  affichage_prochain_button('d', "envoyer_en", "", "Eneyer la clé (e,n) à Bob");
+}
+
+function envoyer_en(){
+  let cours = "lorium isum dot set amet";
+  let resultat = "La clé puclic (e,n) est bien reçus ! e = "+les_cles_dalice[5]+" et n = "+les_cles_dalice[2];
+  resultat += " Bob choisi un mots-clé :";
+  contenue_chaque_button("envoyer_en", cours, resultat);
+
+  let div = document.getElementById("form");
+  if(div.childNodes.length == 0){
+    let form = document.createElement('form')
+    let input = document.createElement('input');
+    input.setAttribute("type", "text");
+    input.setAttribute("id", "mot_cle");
+    input.setAttribute("name", "mot_cle");
+    input.setAttribute("value", "");
+    let button = document.createElement("input");
+    button.setAttribute("type", "button");
+    button.setAttribute("class", "button");
+    button.setAttribute("value", "Confirmer le choix");
+    button.setAttribute("onCLick", "coder_mot()");
+
+    form.appendChild(input);
+    form.appendChild(button);
+    div.appendChild(form);
+  }
+}
+
+function afficherMot_coder(mot_coder){
+  let cours = "lorium isum dot set amet";
+  let resultat = "le mot-clé transformé en nombres selon la position de ses lettres dans l’alphabet  est égale à "+ mot_coder;
+  contenue_chaque_button("codage", cours, resultat);
+  affichage_prochain_button("codage", "chiffrement", mot_coder, "crypter "+ mot_coder+ " et l'envoyer à Alice")
+}
+
+function chiffrement(mot_coder){
+  let chiffrer = square_and_multiply(BigInt(mot_coder), les_cles_dalice[5], les_cles_dalice[2]);
+
+  let cours = "lorium isum dot set amet";
+  let resultat = "le message crypter/chiffré est "+ chiffrer;
+  contenue_chaque_button("chiffrage", cours, resultat);
+
+  let cours2 = "lorium isum dot set amet";
+  let resultat2 = "le message crypter/chiffré que Bob à envoyer est "+ chiffrer;
+  contenue_chaque_button("etape3", cours2, resultat2);
+  affichage_prochain_button("etape3", "dechiffrement", chiffrer, "décrypter "+ chiffrer);
+}
+
+function dechiffrement(chiffrer){
+  let dechiffrer= square_and_multiply(BigInt(chiffrer), les_cles_dalice[4], les_cles_dalice[2]);
+
+  let cours = "lorium isum dot set amet";
+  let resultat = "le message coder en nombre est "+ dechiffrer;
+  contenue_chaque_button("dechiffrage", cours, resultat);
+  affichage_prochain_button("dechiffrage", "decoder_mot", dechiffrer, "decoder "+ dechiffrer+" et afficher le message en lettre");
+}
+
+function afficherMot_decoder(mot){
+  let cours = "lorium isum dot set amet";
+  let resultat = "le message en lettre est "+ mot;
+  contenue_chaque_button("decodage", cours, resultat);
+}
